fa25116f5bfd65e0c4cddd71d6e67e0d4dc09223
jpolstre <jpolstre@gmail.com.com>
2020-07-24 13:40:08 -0400

    Updates

---
+++ precache-manifest.6f284210ccec916b791c35aaf40093e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77fa07017d671a6833a1d6e5b3522970",
+    "url": "/index.html"
+  },
+  {
+    "revision": "620c0965eaa38d199bdb",
+    "url": "/static/js/2.40036283.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.40036283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48ae6668f3c07b17c5f",
+    "url": "/static/js/main.e81bb641.chunk.js"
+  },
+  {
+    "revision": "30a55153835a5bb763ef",
+    "url": "/static/js/runtime-main.ad7cc5ba.js"
+  }
+]);
